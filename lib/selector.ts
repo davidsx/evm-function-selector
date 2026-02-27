44b0551fb6a469c005d8817851a71388632b80cc
David Lau <david.lau@biying.tech>
2026-02-27 15:40:24 +0800

    fix build

--- lib/selector.ts
+++ lib/selector.ts
@@ -1,5 +1,5 @@
-import { keccak_256 } from "@noble/hashes/sha3";
-import { bytesToHex } from "@noble/hashes/utils";
+import { keccak_256 } from "@noble/hashes/sha3.js";
+import { bytesToHex } from "@noble/hashes/utils.js";
 
 /**
  * Normalize an EVM function/error signature to canonical form:
